b37b785334fa5a58e4c73c35fe4adb294f420b1c
CSK <kchandrak2010@gmail.com>
2021-08-25 00:40:25 +0530

    This is a change from another person

---
+++ practise/src/practise/Example2.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git Demo Pull Request");
+		System.out.println("One MOre chane");
+		System.out.println("");
+	}
+
+}
