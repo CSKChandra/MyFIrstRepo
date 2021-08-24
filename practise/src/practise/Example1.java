4df3ba02232623e1aa7149e953ca61890203b04b
CSK <kchandrak2010@gmail.com>
2021-08-25 00:18:39 +0530

    My First Project to Commit

---
+++ practise/src/practise/Example1.java
@@ -0,0 +1,19 @@
+package practise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example1 {
+	
+	public static void main( String [] args) {
+		List <Boolean> list = new ArrayList<>();
+		list.add(true);
+		list.add(Boolean.parseBoolean("FalSe"));
+		list.add(Boolean.TRUE);
+		System.out.println(list.size());
+		System.out.println(list.get(1) instanceof Boolean);
+		
+		
+	}
+
+}
